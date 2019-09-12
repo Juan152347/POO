c22ef27710f64b039b050e5286703451e480c062
Diego <Diego@IBM-Yoga-720-DG>
2019-09-12 16:38:07 -0500

    a

--- ProyectoPE/src/control/GestionCliente.java
+++ ProyectoPE/src/control/GestionCliente.java
@@ -1,9 +1,26 @@
 package control;
 
+import java.util.ArrayList;
+
+import entity.Cliente;
+import entity.Reserva;
+
 public class GestionCliente {
-	
 	public void insertarCliente() {
 		
 	}
-
+	
+	public void eliminarCliente(long cIdentificacion,ArrayList<Cliente>listaClientes,ArrayList<Reserva>reservas){
+		for (Cliente cliente : listaClientes) {
+			if (cIdentificacion==cliente.getNumeroIdentificacion()) {
+				for (Reserva reserva : reservas ) {
+					if(cIdentificacion!=reserva.getCliente().getNumeroIdentificacion()) {
+						//listaClientes.remove(index)
+						System.out.println("la eliminacion ha sido correcta");
+					}
+				}
+			}
+			System.out.println("No existe algun cliente con ese numero de identificacion");
+		}
+	}
 }
