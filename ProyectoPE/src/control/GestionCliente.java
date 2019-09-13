10251201c21429044cb1a9c68133e5d265de11b2
PC <PC@LAPTOP-5M4O54BA>
2019-09-13 10:30:39 -0500

    correccion eliminarcliente

--- ProyectoPE/src/control/GestionCliente.java
+++ ProyectoPE/src/control/GestionCliente.java
@@ -12,33 +12,33 @@ import entity.Tour;
 
 public class GestionCliente {
 
-	public void eliminarCliente(long cIdentificacion,ArrayList<Cliente>listaClientes,ArrayList<Reserva>reservas){
-		boolean b= false;
-		int  cont = 0;
-		for (Cliente cliente : listaClientes){
-			if (cIdentificacion==cliente.getNumeroIdentificacion()) {
-				b=true;	
-				for (Reserva reserva : reservas ) {
-					if(cIdentificacion!=reserva.getCliente().getNumeroIdentificacion()) {
-						listaClientes.remove(cont);
-						System.out.println("la eliminacion ha sido correcta");
-					}
-					else {
-						System.out.println("El cliente esta asociado a una reserva");
+	public void eliminarCliente(long cIdentificacion, ArrayList<Cliente> listaClientes, ArrayList<Reserva> reservas) {
+		Cliente aux = buscarCliente(cIdentificacion, listaClientes);
+		boolean eliminable = true;
+		if (aux != null) {
+			for (Reserva reserva : reservas) {
+				if (aux == reserva.getCliente()) {
+					eliminable = false;
+					System.out.println("no se puede eliminar el cliente");
+				}
+			}
+			if (eliminable) {
+				for (Cliente cliente : listaClientes) {
+					if (aux == cliente) {
+						listaClientes.remove(listaClientes.indexOf(cliente));
+						System.out.println("eliminacion exitosa");
 					}
 				}
-				cont++;
 			}
-			if(b==false) {
-			System.out.println("No existe algun cliente con ese numero de identificacion");
-			}
+		} else {
+			System.out.println("el cliente no existe");
 		}
 	}
 
 	public void ModificarCliente(long ID, ArrayList<Cliente> listaClientes) {
 
 		Scanner x = new Scanner(System.in);
-		int op=0;
+		int op = 0;
 		do {
 
 			if (VerificarExistencia(ID, listaClientes)) {
@@ -55,21 +55,20 @@ public class GestionCliente {
 
 				case 2:
 
-					
 					System.out.println("Ingrese su nuevo numero de identificacion: ");
-					mod(ID,listaClientes,x.nextLong());
+					mod(ID, listaClientes, x.nextLong());
 					break;
 
 				case 1:
 
 					System.out.println("Ingrese su nuevo nombre");
-					mod(ID,x.nextLine(),listaClientes);
+					mod(ID, x.nextLine(), listaClientes);
 					break;
 
 				case 3:
 
 					System.out.println("Ingrese su nuevo numero de contacto");
-					mod(x.nextLine(),ID,listaClientes);
+					mod(x.nextLine(), ID, listaClientes);
 					break;
 				}
 			}
@@ -90,35 +89,49 @@ public class GestionCliente {
 		}
 		return false;
 	}
-    public void mod(long ID,String nombre,ArrayList<Cliente> listaCliente) {
-    	for (Cliente cliente : listaCliente) {
-			if(cliente.getNumeroIdentificacion()==ID) {
+
+	public void mod(long ID, String nombre, ArrayList<Cliente> listaCliente) {
+		for (Cliente cliente : listaCliente) {
+			if (cliente.getNumeroIdentificacion() == ID) {
 				cliente.setNombreCompleto(nombre);
 			}
 		}
-    }
-    public void mod(long ID,ArrayList<Cliente> listaCliente,long IDn) {
-    	boolean idesta=false;
-    	for (Cliente cliente : listaCliente) {
-			if(IDn==cliente.getNumeroIdentificacion()) {
-				idesta=true;
+	}
+
+	public void mod(long ID, ArrayList<Cliente> listaCliente, long IDn) {
+		boolean idesta = false;
+		for (Cliente cliente : listaCliente) {
+			if (IDn == cliente.getNumeroIdentificacion()) {
+				idesta = true;
 			}
 		}
-    	for (Cliente cliente : listaCliente) {
-    		if(cliente.getNumeroIdentificacion()==ID && idesta==false) {
-				cliente.setNumeroIdentificacion(IDn);;
+		for (Cliente cliente : listaCliente) {
+			if (cliente.getNumeroIdentificacion() == ID && idesta == false) {
+				cliente.setNumeroIdentificacion(IDn);
+				;
 			}
 		}
-    }
-    public void mod(String telefono,long ID ,ArrayList<Cliente> listaCliente) {
-    	for (Cliente cliente : listaCliente) {
-			if(cliente.getNumeroIdentificacion()==ID) {
+	}
+
+	public void mod(String telefono, long ID, ArrayList<Cliente> listaCliente) {
+		for (Cliente cliente : listaCliente) {
+			if (cliente.getNumeroIdentificacion() == ID) {
 				cliente.setTelefonoContacto(telefono);
 			}
 		}
-    }
+	}
+
 	public void insertarCliente() {
 		System.out.println("hola que hace");
 
+	}
+
+	public Cliente buscarCliente(long ID, ArrayList<Cliente> listaClientes) {
+		for (Cliente cliente : listaClientes) {
+			if (ID == cliente.getNumeroIdentificacion()) {
+				return cliente;
+			}
+		}
+		return null;
 	}
 }
