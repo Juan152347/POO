fae07a0cf4ff88b16f972ab700aa2471b770fd00
PC <PC@LAPTOP-5M4O54BA>
2019-09-14 15:06:50 -0500

    h

--- ProyectoPE/src/control/ControlAgencia.java
+++ ProyectoPE/src/control/ControlAgencia.java
@@ -19,6 +19,7 @@ public class ControlAgencia {
 	private ArrayList<Tour> listaTours;
 	private ArrayList<Reserva> reservas;
 	private ArrayList<Cliente> listaClientes;
+	private ArrayList<ServicioAdicional> sageneral; 
 
 	public ControlAgencia() {
 		this.listaTours = new ArrayList<>();
@@ -65,7 +66,7 @@ public class ControlAgencia {
 	public void setlClientes(ArrayList<Cliente> lClientes) {
 		this.listaClientes = lClientes;
 	}
-////////////////////////////// VER LISTA DE TOURS ////////////////////////////////////////////////
+
 	public void verlistatours() {
 		for (Tour tour : this.listaTours) {
 			System.out.println(tour.getCodigoIdentidad() + " " + tour.getNombreComercial() + " " + tour.getPrecio());
@@ -86,8 +87,7 @@ public class ControlAgencia {
 		}
 
 	}
-/////////////////////// VER LISTA CLIENTES REGISTRADOS ////////////////////////////////////////////
-	
+
 	public void verlistaclientes() {
 		for (Reserva reserva : this.reservas) {
 			System.out.println(
@@ -96,7 +96,6 @@ public class ControlAgencia {
 		}
 	}
 
-	
 	public void reservarTour() {
 		Scanner sc = new Scanner(System.in);
 		boolean estat = false, estac = false;
@@ -137,7 +136,22 @@ public class ControlAgencia {
 				if (!fval) {
 					System.out.println("no se puede hacer la reserva intente de nuevo");
 				}
+				
 
+			}
+			System.out.println("desea servicios adicionales S/N");
+			char op = sc.next().charAt(0);
+			if (op == 's' || op == 'S') {
+               for(ServicioAdicional sa  : sageneral) {
+            	   System.out.println("codigo del servicio:");
+            	   System.out.println(sa.getServicio());
+            	   System.out.println("Nombre del servicio:");
+            	   System.out.println(sa.getNombreServicio());
+            	   System.out.println("Precio:");
+            	   System.out.println(sa.getPrecio());
+            	   
+            	   
+               }
 			}
 			auxr.setFecha(cal);
 			auxr.setTourReservado(auxt);
@@ -176,25 +190,5 @@ public class ControlAgencia {
 			return false;
 		}
 	}
-///////////////////////////////eliminar reserva (pediente a revision)//////////////////////////////////////////////////
-	public void eliminarReserva (long creserva) {
-		char confirmacion ;
-		boolean existe= false;
-		Scanner esc= new Scanner(System.in);
-		for (Reserva reserva : reservas) {
-			if( creserva==reserva.getNumeroReserva()) {
-				System.out.println("Desea eliminar esta reserva?");
-				confirmacion= esc.next().charAt(0);
-				if (confirmacion=='s') {
-					reservas.remove(reservas.indexOf(reserva));
-					existe= true;
-					System.out.println("La eliminacion de la reserva ha sido correcta");
-				}
-			} 
-		}
-		if (existe == false) {
-			System.out.println("No existe ese numero de reserva");
-		}
-	}
-
+	
 }
